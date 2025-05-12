95408ecee0e3d57380a67ae73990344725c56673
Ale1102 <cesaravendano374@gmail.com>
2025-05-11 21:35:11 -0600

    Cambios en las excepciones y seguridad

---
+++ src/main/java/com/attendance/config/OpenApiConfig.java
@@ -0,0 +1,39 @@
+package com.attendance.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Attendance Management API")
+                        .version("1.0")
+                        .description("API for managing student attendance and grades")
+                        .contact(new Contact()
+                                .name("Support Team")
+                                .email("support@attendance.com")
+                                .url("https://attendance.com/support"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")));
+    }
+}
